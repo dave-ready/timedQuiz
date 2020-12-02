eb5ed8dd86da77182ec0ff4d5b76cc5479f91be8
dave-ready <73320561+dave-ready@users.noreply.github.com>
2020-12-02 14:16:41 -0500

    more pseudo

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Think of the questions you want to ask and multiple answers
-//create basic quiz structure in html & css
+//create basic quiz structure in html & css(index + game files), fonts, colors, sizes
 //create interactive elements in javascript 
-//create variables - questions, choices, answers, score, initials, time left?,
+//create variables - questions, choices, answers, score, initials,(high score section?) time left?,
 //create functions - start quiz(?), timer, 
-//if/else - if (answer === x,y,z, etc.) {....}
+//if/else - if (answer === x,y,z, etc.) {....}, if (answer !== true) {timeSubtract}
 //create a "start quiz" button
 
