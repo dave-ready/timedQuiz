42905425b05117afd5950b954e64fa4566f3f241
dave ready <davis_ready@hotmail.com>
2020-12-02 13:35:57 -0500

    commit with some basic pseudo in js file

---
+++ script.js
@@ -0,0 +1,8 @@
+//Think of the questions you want to ask and multiple answers
+//create basic quiz structure in html & css
+//create interactive elements in javascript 
+//create variables - questions, choices, answers, score, initials, time left?,
+//create functions - start quiz(?), timer, 
+//if/else - if (answer === x,y,z, etc.) {....}
+//create a "start quiz" button
+
