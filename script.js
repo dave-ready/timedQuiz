9245de5747abf173f5034b29507f2fbf91c5314c
dave-ready <73320561+dave-ready@users.noreply.github.com>
2020-12-02 17:51:30 -0500

    create html elements in js file

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
 //Think of the questions you want to ask and multiple answers
-//create basic quiz structure in html & css(index + game files), fonts, colors, sizes
+//create basic quiz structure in html & css
 //create interactive elements in javascript 
-//create variables - questions, choices, answers, score, initials,(high score section?) time left?,
+//style html elements through the DOM
+//create variables - questions, choices, answers, score, initials, time left?,
 //create functions - start quiz(?), timer, 
-//if/else - if (answer === x,y,z, etc.) {....}, if (answer !== true) {timeSubtract}
-//create a "start quiz" button
+//if/else - if (answer === x,y,z, etc.) {....}
+//create a "start quiz" button on the home page
+
+var h1Tag = document.createElement("h1");
+h1Tag.textContent = "Welcome to my quiz!";
+document.body.appendChild(h1Tag);
+
+var pTag = document.createElement("p");
+pTag.textContent = "Press the 'START' button below to begin!";
+document.body.appendChild(pTag);
+
+console.log(h1Tag)
+console.log(pTag)
+
+
+var questOne = "True or False: DOM stands for 'Document Object Model?'";
+var questOneOpt = ["true", "false"];
+var questTwo = "Question two is this";
+var questThree = "Question three is this";
+var questFour = "Question four is this";
 
